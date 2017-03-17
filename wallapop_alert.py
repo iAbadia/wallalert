d8ed35d82d315fdb8f11200a474bda197ef4b3ed
Inaki Abadia <i.abadiaosta@gmail.com>
2017-03-17 18:18:53 +0100

    Changes args input, default values too

--- wallapop_alert.py
+++ wallapop_alert.py
@@ -8,6 +8,16 @@ SAVE_LOCATION = os.path.join(tempfile.gettempdir(), 'alertWallapop.pkl')
 data_save = True
 push_bullet = False
 
+# Query params
+_minPrice = "" # Min query price [0..20000]. Empty for no minPrice
+_maxPrice = "" # Max query price [0..20000]. Empty for no maxPrice
+_dist = "0_" # "0_" -> No limit, "0_10000" -> My town (10km), "0_5000" -> My area (5km), "0_1000" -> Neaby (1km)
+_order = "creationDate-dec" # "distance-asc" -> Near first, "salePrice-asc" -> Cheap first, "salePrice-des" -> Expensive first, "creationDate-des" -> New first
+_kws = "" # Plus-separated query words (E.g. gameboy+color+pokemon)
+_lat = "1.234567" # Latitude in signed degrees format w/ 6 decimals (E.g. 1.234567)
+_lng = "-7.654321" # Longitude in signed degrees format w/ 6 decimals (E.g. -7.654321)
+
+# Notify params
 pushToken = '<your token here>'
 email = '<your email here>'
 
@@ -71,56 +81,72 @@ def wallAlert(urlSearch):
         print('-' * 10)
         if push_bullet:
             sendPushBullet(pushToken, email, title, body, applink)
-
+    print(data_save)
     # Save data
     data_save = open(SAVE_LOCATION, 'wb')
     pickle.dump(data_temp, data_save)
 
 
 def usage():
-    print ("Usage:", __file__," -k <keywords file or list separated by comma>")
+    print ("Usage:", __file__," -k <keywords> -m <min price> -x <max price> -d <distance> -l <latitude> -g <longitude>")
+    optionsHelp()
+
+def optionsHelp():
+    print('     -k : Keywords. E.g. "gameboy pokemon"')
+    print('     -m : Min price [0..20000]. Default: no limit')
+    print('     -x : Max price [0..20000]. Default: no limit')
+    print('     -d : Distance [0_ -> no limit, 0_10000 -> 10km, 0_5000 -> 5km, 0_1000 -> 1km]. Default: no limit')
+    print('     -l : Latitude. Default: 40.4378693')
+    print('     -g : Longitude. Default: -3.819963')
 
 def extractArguments(argv):
-    # Get variables
-    keywordList = []
+
+    _minPrice = ""
+    _maxPrice = ""
+    _dist = "0_"
+    _order = "creationDate-dec"
+    _kws = ""
+    _lat = "40.4378693"
+    _lng = "-3.819963"
 
     try:
-        opts, args = getopt.getopt(argv, "k:", ["keywords="])
+        opts, args = getopt.getopt(argv, "k:m:x:d:l:g:o:")
 
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
     for opt, arg in opts:
-        if opt in ("-k", "--keyword"):
-            # See if is a file
-            try:
-                with open(arg) as f:
-                  keywordList = [item.strip('\n') for item in f.readlines()]
-                # It wasn't a file
-            except:
-                argSplit = arg.split(',')
-                for item in argSplit:
-                    # Remove leading spaces
-                    item = item.strip()
-                    keywordList.append(item)
-
-    if len(keywordList) < 1:
+        _kws = arg if opt in ("-k") else _kws
+        _minPrice = arg if opt in ("-m") else _minPrice
+        _maxPrice = arg if opt in ("-x") else _maxPrice
+        _dist = arg if opt in ("-d") else _dist
+        _order = arg if opt in ("-o") else _order
+        _lat = arg if opt in ("-l") else _lat
+        _lng = arg if opt in ("-g") else _lng
+    
+    if not _kws:
         usage()
-        sys.exit()
+        sys.exit(2)
 
-    return keywordList
-
+    return _kws, _minPrice, _maxPrice, _dist, _order, _lat, _lng
 
 def main(argv):
-    # Process command line arguments
-    keywordList = extractArguments(argv)
 
-    # Loop through keywords
-    for keyword in keywordList:
-        print ("Searching", keyword)
-        urlSearch = 'http://es.wallapop.com/search?kws=' + keyword + '&maxPrice=&dist=0_&order=creationData-des&lat=41.398077&lng=2.170432'
-        wallAlert(urlSearch)
+    # Process command line arguments
+    _kws, _minPrice, _maxPrice, _dist, _order, _lat, _lng = extractArguments(argv)
+
+    print ("Searching", _kws)
+    urlSearch = ('http://es.wallapop.com/search?'
+                'kws=' + _kws + '&'
+                'minPrice=' + _minPrice + '&'
+                'maxPrice=' + _maxPrice + '&'
+                'dist=' + _dist + '&'
+                'order=' + _order + '&'
+                'lat=' + _lat + '&'
+                'lng=' + _lng
+    )
+    wallAlert(urlSearch)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
